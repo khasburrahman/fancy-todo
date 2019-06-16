71c956e1fabe93de04e65be3e81f2689e4384087
Kharis Khasburrahman <khasburrahman@yahoo.com>
2019-06-17 03:02:12 +0700

    add config for heroku

--- config/mongo.config.js
+++ config/mongo.config.js
@@ -1,3 +1,10 @@
+let db_url 
+if (process.env.NODE_ENV === 'production') {
+    db_url = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0-39oye.mongodb.net/test?retryWrites=true&w=majority`
+} else {
+    db_url = `mongodb://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/`
+}
+
 module.exports = {
-    db_url: `mongodb://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/`
+    db_url
 }
